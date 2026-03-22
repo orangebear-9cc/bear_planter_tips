c2683f4b08a7f727bc692f0bd4003bd7f25b3e22
Your Name <your-email@example.com>
2026-03-22 21:08:44 +0800

    1

--- src/config.ts
+++ src/config.ts
@@ -44,7 +44,7 @@ export const siteConfig: SiteConfig = {
 		projects: true, // 项目页面开关
 		skills: false, // 技能页面开关
 		timeline: false, // 时间线页面开关
-		albums: false, // 相册页面开关
+		albums: true, // 相册页面开关
 		devices: false, // 设备页面开关
 	},
 
